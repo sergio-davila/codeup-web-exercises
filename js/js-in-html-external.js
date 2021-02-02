94f174d6f2d6101d66ce41368ee88ea98c274a24
Sergio Davila <sergio.alexander.davila15@gmail.com>
2021-02-02 11:25:19 -0600

    feat: go over js interactive dialog boxes

--- js/js-in-html-external.js
+++ js/js-in-html-external.js
@@ -1,5 +1,17 @@
 "use strict";
 console.log("hello from an external js file.");
 
-name = "Sergio";
-console.log("Hello, " + name);
+var personName = "Sergio";
+console.log("Hello, " + personName);
+
+// Alert
+alert("Welcome to my site!");
+
+//Confirm
+var confirmLeave = confirm("Are you sure you want to leave??");
+console.log("confirmLeave " + confirmLeave);
+
+//Prompt
+var favoriteColor = prompt("What is your favorite color?");
+console.log("Your favorite color is " + favoriteColor);
+
