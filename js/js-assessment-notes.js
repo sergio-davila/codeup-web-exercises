5481cf31427ab6cd23ab07afc07004dfab1227de
Sergio Davila <sergio.alexander.davila15@gmail.com>
2021-04-04 18:37:25 -0500

    feat: finished practice js

--- js/js-assessment-notes.js
+++ js/js-assessment-notes.js
@@ -95,15 +95,18 @@ console.log(printRecipe(caldoRecipe));
 
 function convertAddressToObject(x) {
     var addressNum = '';
-    var address = '';
-    var finalObject = {};
+    var addressString = '';
+    var address = {};
     for (var i = 0; i < x.length; i++) {
         if (x[i] === '0' || x[i] === '1' || x[i] === '2' || x[i] === '3' || x[i] === '4' || x[i] === '5' || x[i] === '6' || x[i] === '7' || x[i] === '8' || x[i] === '9') {
             addressNum = addressNum + x[i];
         } else {
-            address = address + x[i];
+            addressString = addressString + x[i];
         }
     }
+    address.streetNumber = addressNum;
+    address.streetName = addressString;
+    return address;
 }
 
 console.log(convertAddressToObject('8626 Sunny Oaks'));
