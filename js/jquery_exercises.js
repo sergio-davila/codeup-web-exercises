b952158f81ea48a7c262051fd9d0df1ebb00bc4d
Sergio Davila <sergio.alexander.davila15@gmail.com>
2021-04-05 16:38:34 -0500

    feat: starting on jquery selector exercise

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,5 +1,38 @@
 $(document).ready(function () {
     console.log("fully loaded and ready to go!")
+
+    // ID selectors below
+
+
+
+    // Class selectors below
+
+    // Element selectors(exercise) below
+
+
+    // Element selectors examples below vvv
+    // $('p').css({'font-size':'30px', 'backgroundColor':'purple', 'color' : 'fuchsia'});
+    // $('li').css({'font-size':'70px', 'color':'rebeccapurple'});
+    // $('h1').css('color', 'blue');
+    // $('div').css({'background-color':'lavender', 'font-size':'50px', 'color':'red'});
+    // $('body').css('background-color', 'lightgrey');
+    // $('div').html('New text inside of my div tag, courtesy of jquery!');
+    // $('p').html('This is brand new text in my p tag, cus jquery replaced it!');
+
+    // all selector example below
+    $('*').css({'font-style':'italic', 'font-family':'sans-serif'});
+
+    // this ones neat, courtesy of Victor
+    // $("*").keyup(function(){
+    //     $('*').hide()
+    // })
+    // $('*').click(function(){
+    //     $('*').show()
+    // })
 });
+
+
+
+
 
 console.log("hello jQuery!");
