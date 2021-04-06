867e1c3ca94fa1a87fe3c71d16b6b278d9fa335c
Sergio Davila <sergio.alexander.davila15@gmail.com>
2021-04-06 13:58:52 -0500

    feat: refactored code

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -2,9 +2,9 @@ $(document).ready(function () {
     console.log("fully loaded and ready to go!")
 
     //example with outside function
-    function h1Click(e) {
-        console.log(e);
-        $(e.target).css('background', 'limegreen');
+    function h1Click() {
+        // console.log(e);
+        $(this).css('background', 'limegreen');
     }
 
     $('h1').click(h1Click);
@@ -17,11 +17,12 @@ $(document).ready(function () {
 
     //example with multiple functions using BOTH outside and inside
     function hovering(e) {
-        console.log(e);
+        console.log('enter');
         $(e.target).css('color', 'red');
     }
 
     $('li').hover(hovering, function() {
+        console.log('leave');
         $(this).css('color', 'black');
     });
 
