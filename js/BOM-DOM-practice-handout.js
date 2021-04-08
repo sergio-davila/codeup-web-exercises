340a8351805ce93c953d7c9e148224b4c42aab8a
Sergio Davila <sergio.alexander.davila15@gmail.com>
2021-04-08 11:13:42 -0500

    feat: working on ajax lec

---
+++ js/BOM-DOM-practice-handout.js
@@ -0,0 +1,15 @@
+//TODO: When the #changeBoxSize button is clicked the box should double in size from what it was!
+
+//TODO: I have two CSS classes - toggleClass1 and toggleClass1 - that are currently unused. When the #toggleClass button is clicked, program a bit of JavaScript to toggle between those classes to see the background-color switch back and forth with each click. Bonus: Write your callback function in a variable and use it instead!
+
+//TODO: In the #boxInfo element, when the BOX is hovered over the HEIGHT and WIDTH of the box would appear in that HTML area [div]
+
+//TODO: When the user types in to our #input and then clicks #inputBtn, what they typed into the box should replace what was inside of the span #userName with the input's contents
+
+//TODO: Refactor the above work - after 5 seconds have passed from the user clicking #inputBtn and seeing #userName change, change the entire div of #helloUser to say "Let me know if you want to play a game. ."
+
+
+
+
+
+
