a803b2088db9a8380d2ebbfa85b70dd43e19f84e
Sergio Davila <sergio.alexander.davila15@gmail.com>
2021-04-22 14:33:21 -0500

    feat: used help.js for the quiz

---
+++ js/help.js
@@ -0,0 +1,39 @@
+// function sayHello(name) {
+//     return 'Hello, ' + name + '!';
+// }
+//
+// console.log(sayHello("eyyr"));
+//
+// const sayHello2 = (name) => { 'Hello, ' + name + '!' }
+//
+// console.log(sayHello2("resft"));
+//
+// const sayHello3 = name => { 'Hello, ' + name + '!' }
+//
+// console.log(sayHello3("srgsfg"));
+//
+//
+// const sayHello4 = (name) => `Hello, ${name}!`
+//
+// console.log(sayHello4("sfdgsdf"));
+
+// let numbers = [1, 2, 3, 4, 5];
+// let newNum = numbers.map(n => n * 3)
+//
+// console.log(numbers);
+//
+// console.log(newNum);
+
+// const isDivisibleBy3 = n => n % 3 === 0;
+//
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//
+// console.log(numbers.filter(isDivisibleBy3));
+// console.log(numbers);
+
+// const p = new Promise((resolve, reject) => {
+//     resolve(3)
+// });
+//
+// p.then(result => console.log(result));
+// p.catch(error => console.log('An error occurred!'));
